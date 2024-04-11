5633b7cba2b84a1b8b1eee63a41c6723d0a4b27c
Saish <saishnanote3117@gmail.com>
2024-04-11 15:50:53 +0530

    Copy UI from old App

--- app/src/main/java/com/example/rescuemitraapp/MainActivity.kt
+++ app/src/main/java/com/example/rescuemitraapp/MainActivity.kt
@@ -1,20 +1,40 @@
-package com.example.rescuemitraapp
+package com.example.rescuemitraapps
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.rescuemitraapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Find the button by its ID
+        val buttonNavigateToSecondActivity = findViewById<Button>(R.id.button)
+
+        // Set an OnClickListener for the button
+        buttonNavigateToSecondActivity.setOnClickListener {
+            // Create an Intent to navigate to SecondActivity
+            val intent = Intent(this, second::class.java)
+            startActivity(intent)
         }
+
+        // Delay the navigation to SecondActivity for 2 seconds
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, second::class.java)
+            startActivity(intent)
+            finish() // Finish MainActivity to prevent going back to it using the back button
+        }, 2000) // 2000 milliseconds = 2 seconds
     }
+}
+
+class SecondActivity {
+
 }
