abfba38f3979f2b71a070d0f66e957a3deb37d4a
Saish <saishnanote3117@gmail.com>
2024-04-11 19:05:18 +0530

    Connected to Firebase and Added SignIN & SignUP through Firebase

--- app/src/main/java/com/example/rescuemitraapp/Login.kt
+++ app/src/main/java/com/example/rescuemitraapp/Login.kt
@@ -4,36 +4,64 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 
 class Login : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
 
-        val button1 = findViewById<Button>(R.id.signin)
-        val button2 = findViewById<Button>(R.id.gethelp)
-        val button3 = findViewById<Button>(R.id.geelp)
+        firebaseAuth = FirebaseAuth.getInstance()
 
-        button1.setOnClickListener {
-            val intent = Intent(this, Rescue::class.java)
-            startActivity(intent)
+        val signInButton: Button = findViewById(R.id.sign_in)
+        val button2: Button = findViewById(R.id.gethelp)
+        val button3: Button = findViewById(R.id.geelp)
 
-
-            button2.setOnClickListener {
-                // Handle Sign Up button click
-                val intent = Intent(this, Regsiter::class.java)
-                startActivity(intent)
-                button3.setOnClickListener {
-                    // Handle Sign In button click
-                    val intent = Intent(this,Regsiter ::class.java)
-                    startActivity(intent)
+        val emailEditText: EditText = findViewById(R.id.editTextEmail)
+        val passwordEditText: EditText = findViewById(R.id.password)
 
 
-                }
+        signInButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val pass = passwordEditText.text.toString()
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+                firebaseAuth.signInWithEmailAndPassword(email, pass)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(this, "Sign In Successful", Toast.LENGTH_SHORT).show()
+
+                            val intent = Intent(this, Rescue::class.java)
+                            startActivity(intent)
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(this, "Sign In Failed", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            } else {
+                Toast.makeText(this, "Error : Email or Password Field Is Empty", Toast.LENGTH_SHORT)
+                    .show()
             }
         }
+
+        button2.setOnClickListener {
+            // Handle Sign Up button click
+            val intent = Intent(this, Regsiter::class.java)
+            startActivity(intent)
+
+        }
+
+        button3.setOnClickListener {
+            // Handle Sign In button click
+            val intent = Intent(this, Regsiter::class.java)
+            startActivity(intent)
+        }
     }
+
 }
--- app/src/main/java/com/example/rescuemitraapp/Regsiter.kt
+++ app/src/main/java/com/example/rescuemitraapp/Regsiter.kt
@@ -4,28 +4,50 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class Regsiter : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
+    private lateinit var firebaseAuth: FirebaseAuth;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register)
+        firebaseAuth = FirebaseAuth.getInstance()
+        val signInButton: Button = findViewById(R.id.sign_in)
+        val signUpButton: Button = findViewById(R.id.sign_up)
+        val emailEditText: EditText = findViewById(R.id.editTextEmail)
+        val passwordEditText: EditText = findViewById(R.id.password)
 
-        val button1 = findViewById<Button>(R.id.SinIn)
-        val button2 = findViewById<Button>(R.id.signini)
-
-        button1.setOnClickListener {
+        signInButton.setOnClickListener {
             val intent = Intent(this, Rescue::class.java)
             startActivity(intent)
+        }
 
-
-            button2.setOnClickListener {
-                // Handle Sign Up button click
-                val intent = Intent(this, Login::class.java)
-                startActivity(intent)
-
+        signUpButton.setOnClickListener {
+            /*// Handle Sign Up button click
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)*/
+            val email = emailEditText.text.toString()
+            val pass = passwordEditText.text.toString()
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+                firebaseAuth.createUserWithEmailAndPassword(email, pass)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(this, "Sign Up Successful", Toast.LENGTH_SHORT).show()
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(this, "Sign Up Failed", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            } else {
+                Toast.makeText(this, "Error : Email or Password Field Is Empty", Toast.LENGTH_SHORT).show()
             }
+
+
         }
     }
 }
